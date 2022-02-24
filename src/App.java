c8fdd2d4412633294971f800de22fd7e67fb67c8
BaoziBandit <kakkigoori@gmail.com>
2022-02-23 20:45:53 -0700

    Moved Min Finder it's own class.

--- src/App.java
+++ src/App.java
@@ -1,43 +1,6 @@
-import java.util.*;
-
 public class App {
     public static void main(String[] args){
-        int[] numbers = readIntegers();
-        System.out.println("Min : " + findMin(numbers));
+        int[] numbers = MinFinder.readIntegers();
+        System.out.println("Min : " + MinFinder.findMin(numbers));
     } 
-
-    public static int[] readIntegers(){
-        Scanner scan = new Scanner(System.in);
-        int[] nums = new int[0];
-
-        while(scan.hasNextInt()){
-            int newNum = scan.nextInt();
-            int[] newArray = new int[nums.length + 1];
-            for(int i=0; i < newArray.length; i++)
-            {
-                if(i == nums.length)
-                {
-                    newArray[i] = newNum;
-                }
-                else
-                    newArray[i] = nums[i];
-            }
-            nums = newArray;
-        }
-
-        scan.close();
-        return nums;
-    }
-
-    public static int findMin(int[] nums){
-        int min = nums[0];
-        for(int i = 1; i < nums.length; i++)
-        {
-            if(nums[i] < min)
-            {
-                min = nums[i];
-            }
-        }
-        return min;
-    }
 }
---
+++ src/MinFinder.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class MinFinder {
+    public static int[] readIntegers(){
+        Scanner scan = new Scanner(System.in);
+        int[] nums = new int[0];
+
+        while(scan.hasNextInt()){
+            int newNum = scan.nextInt();
+            int[] newArray = new int[nums.length + 1];
+            for(int i=0; i < newArray.length; i++)
+            {
+                if(i == nums.length)
+                {
+                    newArray[i] = newNum;
+                }
+                else
+                    newArray[i] = nums[i];
+            }
+            nums = newArray;
+        }
+
+        scan.close();
+        return nums;
+    }
+
+    public static int findMin(int[] nums){
+        int min = nums[0];
+        for(int i = 1; i < nums.length; i++)
+        {
+            if(nums[i] < min)
+            {
+                min = nums[i];
+            }
+        }
+        return min;
+    }
+}
